2492e9af5f83b454b0e0652cb5f4db535cca47d8
Rafael Martins Paiva <rafael.martins.paiva0@gmail.com>
2025-10-24 11:30:32 -0300

    .

--- components/test.tsx
+++ components/test.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-// Define a interface para as props, neste caso, o conteúdo inicial é opcional.
 interface EditableDivProps {
   initialContent?: string;
 }
@@ -14,19 +13,14 @@ const EditableDiv: React.FC<EditableDivProps> = ({  }) => {
     <div style={{ padding: '20px' }}>
       <h2>Editor de Conteúdo Flexível</h2>
       
-      {/* O elemento DIV Editável */}
+      {}
       <div
         id="editor"
-        contentEditable="true" // Torna o conteúdo editável
-        
-
+        contentEditable="true"
         
         style={{ 
-          border: '1px solid #0070f3', 
           padding: '15px', 
           minHeight: '150px', 
-          borderRadius: '6px',
-          backgroundColor: '#f9f9f9',
           cursor: 'text' 
         }}
       />
